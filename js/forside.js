da4ddc9ac8e06af098eec30b5a2940d4f7a0c21a
NikolineSchmidt <eaa23nsch@students.eaaa.dk>
2025-02-07 17:14:04 +0100

    burgermenu til kryds

--- js/forside.js
+++ js/forside.js
@@ -1,9 +1,9 @@
-// script.js
-document.addEventListener('DOMContentLoaded', function() {
-    const burger = document.querySelector('.burger');
-    const folderTabs = document.getElementById('folder-tabs');
+document.addEventListener("DOMContentLoaded", function () {
+    const burger = document.querySelector(".burger");
+    const folderTabs = document.querySelector(".folder-tabs");
 
-    burger.addEventListener('click', function() {
-        folderTabs.classList.toggle('active');
+    burger.addEventListener("click", function () {
+        folderTabs.classList.toggle("active");
+        burger.classList.toggle("active");
     });
 });
